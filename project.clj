817fc313bdbb3e3fb5e4ad3d86add7e2b961c25d
Mandimby RAVELOARINJAKA <mandimby.raveloarinjaka@gmail.com>
2019-03-17 20:48:56 -0400

    Version 1.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shadow-cljs-app/lein-template "1.0.0"
+(defproject shadow-cljs-app/lein-template "1.0.1-SNAPSHOT"
   :description "boilerplate to setup a clojurescript project built using shadow-cljs"
   :url "https://github.com/mraveloarinjaka/cljs-app"
   :license {:name "Eclipse Public License"
