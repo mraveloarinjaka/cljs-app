da37f922c5a268897325c0be48a489908bfda9fc
Mandimby RAVELOARINJAKA <mandimby.raveloarinjaka@gmail.com>
2021-03-21 12:55:06 -0400

    [FIX] (lein): use credentials from environment variables

--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
   :description "boilerplate to setup a clojurescript project built using shadow-cljs"
   :url "https://github.com/mraveloarinjaka/cljs-app"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-2.0"}
-  :repositories [["releases" {:url "https://repo.clojars.org"
-                               :username [:env/clojars_username]
-                               :password [:env/clojars_password]}]
-                 ["snapshots" {:url "https://repo.clojars.org"
-                               :username [:env/clojars_username]
-                               :password [:env/clojars_password]}]]
+            :url  "http://www.eclipse.org/legal/epl-2.0"}
+  :repositories [["releases" {:url      "https://repo.clojars.org"
+                              :username :env/clojars_username
+                              :password :env/clojars_password}]
+                 ["snapshots" {:url      "https://repo.clojars.org"
+                               :username :env/clojars_username
+                               :password :env/clojars_password}]]
   :eval-in-leiningen true)
